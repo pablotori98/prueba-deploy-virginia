5371ba32fbae73241879fa6083f23c704b0dd1de
pablotori98 <pablotori98@gmail.com>
2023-02-15 11:07:16 +0000

    login añadido sin estilos

--- src/front/js/pages/Login.js
+++ src/front/js/pages/Login.js
@@ -1,12 +1,59 @@
 //Import React
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 
 //Import materials
-import { Box, Typography } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
+import { Formik, useFormik } from "formik";
+import { CallToActionSharp } from "@mui/icons-material";
 
-export const Login = () =>{
-return (
-    <Box>login</Box>
-)
-}
+//Import context
+import { Context } from "../store/appContext"
+
+export const Login = () => {
+    const {actions, store} = useContext(Context)
+  const onSubmit = async (values, ax) => {
+    await actions.signup(
+      values.email,
+      values.password
+    );
+  };
+
+  const { values, handleSubmit, handleChange, onChange } = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit,
+  });
+  console.log(values);
+
+  return (
+    <Box>
+      <Box>
+        <Typography>Sign Up text</Typography>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            values={values.email}
+            onChange={handleChange}
+            name="email"
+            label="Email"
+          />
+          <TextField
+            values={values.password}
+            onChange={handleChange}
+            type="password"
+            name="password"
+            label="Contraseña"
+          />
+          <Button
+          type="submit"
+          variant="contained"
+          >
+            Registro
+          </Button>
+        </form>
+      </Box>
+    </Box>
+  );
+};
