26759c51f575dc2c72a286e78d450c63203c5f1e
pablotori98 <pablotori98@gmail.com>
2023-02-18 12:43:14 +0000

    pagina contact front finalizado

--- src/front/js/pages/ContactInfo.js
+++ src/front/js/pages/ContactInfo.js
@@ -5,8 +5,16 @@ import { Link } from "react-router-dom";
 //Import materials
 import { Box, Typography } from "@mui/material";
 
+//Import Image
+import pluma from "../assets/forms/pluma.png"
+import { ContactHome } from "../components/contacthome/ContactHome.jsx";
 export const ContactInfo = () =>{
     return(
-        <Box>Contact</Box>
+        <Box>
+            <ContactHome />
+            <Box className="fatherpluma">
+            <img className="plumacontact" src={pluma} />
+            </Box>
+        </Box>
     )
 }
