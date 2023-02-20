835f3241f36a641ed7bbfdad2880da707bd3edfb
pablotori98 <pablotori98@gmail.com>
2023-02-20 20:01:15 +0000

    sobre mi mobile

--- src/front/js/components/aboutme/aboutmespanish/AboutMeSpanishMobile.jsx
+++ src/front/js/components/aboutme/aboutmespanish/AboutMeSpanishMobile.jsx
@@ -3,7 +3,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 //Import materials
-import { Box, Typography } from "@mui/material";
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Box } from '@mui/material';
+
 import imagenpsicologa from "../../../assets/psicologa/psicologaimagen.jpg";
 
 export const AboutMeSpanishMobile = () => {
@@ -30,36 +36,61 @@ export const AboutMeSpanishMobile = () => {
 
       <Box className="contentaboutmemobile">
       <h1 className="fontabhaya aboutmetitle">Sobre Mi</h1>
-        <Box className="">
-          <Box className=" text-center">
-            <h3 className="fontabhaya">Especialidades</h3>
-            <p>Especialidad 1</p>
-            <p>Especialidad 2</p>
-            <p>Especialidad 3</p>
+      <Box className="m-2">
+      <Accordion sx={{backgroundColor:"#F5ECE8"}}>
+        <AccordionSummary sx={{backgroundColor:"#E1BAAC"}}
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography className='text-white'><strong>Especialidades</strong></Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography className='m-2'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      </Box>
+      <Box className="m-2">
+      <Accordion sx={{backgroundColor:"#F5ECE8"}}>
+        <AccordionSummary sx={{backgroundColor:"#E1BAAC"}}
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography className='text-white'><strong>Estudios</strong></Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+        <Typography className='m-2'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      </Box>
 
-          </Box>
-          <Box className=" text-center">
-            <h3 className="fontabhaya">Estudios</h3>
-            <p>Estudios 1</p>
-            <p>Estudios 2</p>
-            <p>Estudios 3</p>
-          </Box>
-          <Box className=" text-center">
-            <h3 className="fontabhaya">Información adicional</h3>
-            <p>Información adicional 1</p>
-            <p>Información adicional 2</p>
-            <p>Información adicional 3</p>
-          </Box>
-          <Box className=" text-center">
-            <h3 className="fontabhaya">Información adicional</h3>
-            <p>Información adicional 4</p>
-            <p>Información adicional 5</p>
-            <p>Información adicional 6</p>
-          </Box>
+      <Box className="m-2">
+      <Accordion sx={{backgroundColor:"#F5ECE8"}}>
+        <AccordionSummary sx={{backgroundColor:"#E1BAAC"}}
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography className='text-white'><strong>Información adicional</strong></Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+        <Typography className='m-2'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      </Box>
         </Box>
       </Box>
 
-      </Box>
   );
 };
 
