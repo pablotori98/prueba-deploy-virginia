a7bbb5d6515db19d9bf7c12a43f0b21e9587ae24
pablotori98 <pablotori98@gmail.com>
2023-02-21 11:54:01 +0000

    blogs individuales ya muestran la imagen

--- src/front/js/components/blog/spanish/OnlyPostSpanish.jsx
+++ src/front/js/components/blog/spanish/OnlyPostSpanish.jsx
@@ -22,7 +22,7 @@ export const OnlyPostSpanish = () => {
             <Box className="blogpost mb-5">
               <img
                 className="imgpost"
-                src="https://img.freepik.com/foto-gratis/mujer-manos-juntas-hablando-consejero_23-2148759093.jpg?w=1380&t=st=1676660251~exp=1676660851~hmac=e5dc87333d7281a3a1b947ecde6ad9fdba67a84d9b7a12dc487749fb76e12abe"
+                src={store.singlepost.image_post}
               />
               <Typography variant="h3" className="p-2">
                 {store.singlepost.title_post}
