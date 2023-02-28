420ba6c0aee66b5f8b162fc9c291ca061960ac45
pablotori98 <pablotori98@gmail.com>
2023-02-28 12:42:21 +0000

    actualizacion estilos pago

--- src/front/js/components/purchases/SessionPurchase.jsx
+++ src/front/js/components/purchases/SessionPurchase.jsx
@@ -2,51 +2,71 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
-
+import imagenpago from "../../assets/forms/pago.png";
 //Import materials
-import { Box, MenuItem, Select, Typography, FormControl, InputLabel } from "@mui/material";
+import {
+  Box,
+  MenuItem,
+  Select,
+  Typography,
+  FormControl,
+  InputLabel,
+  Button,
+} from "@mui/material";
 import { PayPal } from "../PayPal/PayPal";
-import { Context } from "../../store/appContext"
+import { Context } from "../../store/appContext";
 export const SessionPurchase = () => {
-  const {actions, store} = useContext(Context)
-    const [sessions, setSessions] = useState(1)
-    const [price, setPrice] = useState()
+  const { actions, store } = useContext(Context);
+  const [sessions, setSessions] = useState(1);
+  const [price, setPrice] = useState();
+  const [open, setOpen] = useState(false);
+  const handleChange = (event) => {
+    setSessions(event.target.value);
+  };
+  useEffect(() => {
+    actions.setPrice((sessions * 50).toFixed(1));
+  }, [sessions]);
 
-    const handleChange = (event) => {
-        setSessions(event.target.value);
-      };
-      useEffect(()=>{actions.setPrice((sessions * 50).toFixed(1))
-      },[sessions])
-
-      
   return (
     <Box className="fatherpurchase">
       <Box className="leftpurchase">
-        Comprar Sesiones
-
-        <FormControl>
-        <InputLabel id="demo-simple-select-label">Age</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={sessions}
-          label="sessions"
-          onChange={handleChange}
-        >
-          <MenuItem value={1}>1 Sesión</MenuItem>
-          <MenuItem value={2}>2 Sesiones</MenuItem>
-          <MenuItem value={3}>3 Sesiones</MenuItem>
-          <MenuItem value={4}>4 Sesiones</MenuItem>
-          <MenuItem value={5}>5 Sesiones</MenuItem>
-        </Select>
-      </FormControl>
+        <img className="leftimagepurchase" src={imagenpago} />
       </Box>
 
       <Box className="rightpurchase">
-        <Box>
-            <Typography>Precio {store.price}€</Typography>
+        <p className="fontabhaya my-3 fontsize3rem">Sesiones terapia</p>
+
+        <FormControl className="my-4 w-50">
+          <InputLabel id="demo-simple-select-label">Sesiones</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={sessions}
+            label="sessions"
+            onChange={handleChange}
+          >
+            <MenuItem value={1}>1 Sesión</MenuItem>
+            <MenuItem value={2}>2 Sesiones</MenuItem>
+            <MenuItem value={3}>3 Sesiones</MenuItem>
+            <MenuItem value={4}>4 Sesiones</MenuItem>
+            <MenuItem value={5}>5 Sesiones</MenuItem>
+          </Select>
+        </FormControl>
+        <Box className="w-50">
+          <p className="fontabhaya text-center fontsize2rem">
+            Precio {store.price}€
+          </p>
+
+
+
+          {open == true ? <PayPal /> :           <Button
+            onClick={() => setOpen(true)}
+            variant="contained"
+            className="paybutton w-100"
+          >
+            Pagar
+          </Button>}
         </Box>
-        <PayPal />
       </Box>
     </Box>
   );
